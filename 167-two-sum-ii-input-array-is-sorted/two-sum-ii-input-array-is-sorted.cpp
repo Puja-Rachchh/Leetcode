3cedf6e6f4d8c59ee7b0454d704be215d5f2e1ad
Puja Rachchh <143953428+Puja-Rachchh@users.noreply.github.com>
2025-12-22 12:28:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (63.26%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-    int binary_search(int k,int start,int end,vector<int>& numbers){
-        
-        int mid=(start+end)/2;
-        while(start<=end){
-            if(numbers[mid]==k) return mid;
-            else if(numbers[mid]>k){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-            }
-            mid=(start+end)/2;
-        }
-        return -1;
-    }
     vector<int> twoSum(vector<int>& numbers, int target) {
         vector<int> res;
-        int n=numbers.size();
-        for(int i=0;i<n;i++){
-            int k =target-numbers[i];
-            int ans = binary_search(k,i+1,n-1,numbers);
-            if(ans!=-1){
-                res.push_back(i+1);
-                res.push_back(ans+1);
-            }
+        int j=numbers.size()-1;
+        int i=0;
+        while(numbers[i]+numbers[j]!=target){
+            if(numbers[i]+numbers[j]>target) j--;
+            else{ i++;}
         }
+        res.push_back(i+1);
+        res.push_back(j+1);
         return res;
     }
 };
