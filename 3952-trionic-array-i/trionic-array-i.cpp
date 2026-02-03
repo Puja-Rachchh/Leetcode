8e1dc0dd4fde1c5903a063e2e72442560727b53e
Puja Rachchh <143953428+Puja-Rachchh@users.noreply.github.com>
2026-02-03 12:44:36 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (69.85%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.cpp
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -6,18 +6,10 @@ public:
         int z=0;
         for(int i=1;i<nums.size();i++){
             if(nums[i]>nums[i-1] && q==0) p=i;
-            else if(nums[i]<nums[i-1]&& z==0){
-                q=i;
-            }
-            else if(nums[i]>nums[i-1]&& p!=0){
-                z=i;
-                
-            }
-            else{
-                return false;
-            }
+            else if(nums[i]<nums[i-1]&& z==0) q=i;
+            else if(nums[i]>nums[i-1]&& p!=0) z=i;   
+            else return false;
         }
-        cout<<p<<q<<z;
         return 0<p && p<q && q<z;
     }
 };
