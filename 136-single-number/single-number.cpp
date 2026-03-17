0d973840337ca24392575a37234ae75e77d3a57b
Puja Rachchh <143953428+Puja-Rachchh@users.noreply.github.com>
2026-03-17 22:22:37 +0530

    Time: 11 ms (13.83%) | Memory: 23.9 MB (7.62%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> freq;
+        unordered_map<int,int> freq;
         for(int i=0;i<nums.size();i++){
             freq[nums[i]]++;
         }
-        for(auto& it:freq){
+        for(auto it:freq){
             if(it.second==1){
                 return it.first;
             }
